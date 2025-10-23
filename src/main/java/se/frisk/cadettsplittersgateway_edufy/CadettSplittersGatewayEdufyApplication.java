d791c1036cdd5ed2a94d53169393e2b66dcbe55c
tiefr <tie.frisk@hotmail.com>
2025-10-23 13:40:54 +0200

    Initial commit

---
+++ src/main/java/se/frisk/cadettsplittersgateway_edufy/CadettSplittersGatewayEdufyApplication.java
@@ -0,0 +1,13 @@
+package se.frisk.cadettsplittersgateway_edufy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadettSplittersGatewayEdufyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadettSplittersGatewayEdufyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/frisk/cadettsplittersgateway_edufy/CadettSplittersGatewayEdufyApplicationTests.java
@@ -0,0 +1,13 @@
+package se.frisk.cadettsplittersgateway_edufy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadettSplittersGatewayEdufyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
